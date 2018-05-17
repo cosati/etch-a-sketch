ecf3c4f258bac7b81b41deba53ff51be475b214e
cosati <cosati@hotmail.com>
2018-05-17 13:36:42 -0300

    Index, Style, Script
    Cor, reset, alterar tamanho grid

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,41 @@
-const boxPerLine = 16;
-const boxPerColumn = 16;
+let boxPerSide = 16;
+let selectedColor = "black";
 
 window.addEventListener('DOMContentLoaded', function () {
 	const container = document.querySelector('#container');
+	createBoxes();
 });
 
+function paintBox(e) {
+	e.target.style.backgroundColor = selectedColor;
+}
+
 function createBoxes() {
+  container.innerHTML = '';
   let blocks = [];
-  let q = boxPerColumn * boxPerLine;
+  let q = boxPerSide*boxPerSide;
+  let size = 800/boxPerSide;
+  console.log(size);
   for (let i = 0; i < q; i++) {
     blocks.push(document.createElement('div'));
   }
-  console.log(blocks);
   blocks.forEach(function(item) {
 	item.classList.add('block');
+	item.style.flex = "1 1 " + size + "px"; 
+	item.addEventListener('mouseover', paintBox);
     container.appendChild(item);
   });
 }
 
+function reset() {
+	let blocks = document.getElementById("container").getElementsByTagName('div');
+	let boxes = Array.from(blocks);
+	console.log(boxes);
+	boxes.forEach(function(box) {
+		box.style.backgroundColor = "";
+	});
+}
+
+function selectColor(color) {
+	selectedColor = color;
+}
