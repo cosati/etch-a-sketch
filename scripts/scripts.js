64faf25a860a686151c6666f7d0a0625dd51bdec
Alexandre Cosati <cosati.alexandre@gmail.com>
2018-05-19 12:08:09 -0300

    Commit final

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,14 +14,14 @@ function createBoxes() {
   container.innerHTML = '';
   let blocks = [];
   let q = boxPerSide*boxPerSide;
-  let size = 800/boxPerSide;
+  let size = Math.floor(800/boxPerSide);
   console.log(size);
   for (let i = 0; i < q; i++) {
     blocks.push(document.createElement('div'));
   }
   blocks.forEach(function(item) {
 	item.classList.add('block');
-	item.style.flex = "1 1 " + size + "px"; 
+	item.style.flex = "1 1 " + size + "px";
 	item.addEventListener('mouseover', paintBox);
     container.appendChild(item);
   });
@@ -34,8 +34,26 @@ function reset() {
 	boxes.forEach(function(box) {
 		box.style.backgroundColor = "";
 	});
+  document.querySelector('.alert').style.display = "none";
 }
 
 function selectColor(color) {
 	selectedColor = color;
+}
+
+function plotCanvas(form) {
+  if (form[0].value < 0) {
+    document.querySelector('.alert').innerHTML = "Only positive numbers!"
+    document.querySelector('.alert').style.display = "block";
+    return;
+  }
+  boxPerSide = (form[0].value > 100 ? 100 : form[0].value);
+  if (form[0].value > 100) {
+    document.querySelector('.alert').innerHTML = "Maximum is 100!"
+    document.querySelector('.alert').style.display = "block";
+    document.querySelector('.number').value = "100";
+  } else {
+    document.querySelector('.alert').style.display = "none";
+  }
+  createBoxes();
 }
